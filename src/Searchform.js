b9895dd53c2c1816de8d48dbedf0f2a78ddbe7d8
👩Ladybug🐞 <psyjulga@yahoo.de>
2021-08-28 20:23:15 +0200

    worked on searchform and todayweather

--- src/Searchform.js
+++ src/Searchform.js
@@ -3,24 +3,27 @@ import axios from "axios";
 import "./Searchform.css";
 
 export default function Searchform() {
+  let [cityInput, setCityInput] = useState(false);
+
   let [city, setCity] = useState(" ");
+
   let [weather, setWeather] = useState({});
 
-  let [loaded, setLoaded] = useState(false);
-
   function displayWeather(response) {
-    setLoaded(true);
     setWeather({
-      temp: response.data.main.temp,
+      cityOutput: response.data.name,
+      temp: Math.round(response.data.main.temp),
+      date: "Sunday 16:24",
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
-      wind: response.data.wind.speed,
+      wind: Math.round(response.data.wind.speed),
       icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
+
+    setCityInput(true);
   }
 
-  function startAxiosCall(event) {
-    event.preventDefault();
+  function startAxiosCall(city) {
     let unit = "metric";
     let apiKey = "521e636942417dbc233358cdf12445eb";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
@@ -28,19 +31,25 @@ export default function Searchform() {
     axios.get(apiUrl).then(displayWeather);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    startAxiosCall(city);
+  }
+
   function updateCity(event) {
     setCity(event.target.value);
   }
 
   let form = (
-    <form className="Searchform m-4" onSubmit={startAxiosCall}>
+    <form className="Searchform m-4" onSubmit={handleSubmit}>
       <div className="row">
         <div className="city-input-col col-md-9">
           <input
             type="search"
             placeholder="Enter a city ..."
             className="form-control"
-            autoComplete="off"
+            autoComplete={false}
             autoFocus={true}
             onChange={updateCity}
           />
@@ -56,23 +65,53 @@ export default function Searchform() {
     </form>
   );
 
-  if (loaded) {
-    return (
-      <div>
-        {form}
+  let todayweather = (
+    <div className="Todayweather row">
+      <div className="col-sm-2">
+        <img src={weather.icon} alt="weather-icon" className="icon" />
+      </div>
+
+      <div className="output-one col-sm-6">
+        <div className="output-city-temp-unit">
+          <span className="city-and-temp">
+            <span>{weather.cityOutput} </span>
+            <span>{weather.temp}</span>°
+          </span>
+          <a href="/" className="celsius link active" title="Celsius Unit">
+            C
+          </a>
+          <span className="pipe">|</span>
+          <a href="/" className="fahrenheit link" title="Fahrenheit Unit">
+            F
+          </a>
+        </div>
+        <div className="date">{weather.date}</div>
+      </div>
+
+      <div className="col-sm-4 output-two">
         <ul>
-          <li>Temperature: {Math.round(weather.temp)}°C</li>
-          <li className="description">Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {Math.round(weather.wind)} km/h</li>
-          <li>
-            {" "}
-            <img src={weather.icon} alt={weather.description} />
-          </li>
+          <li className="wind">Windspeed: {weather.wind} km/h</li>
+          <li className="humidity">Humidity: {weather.humidity} %</li>
+          <li className="description">{weather.description}</li>
         </ul>
+      </div>
+    </div>
+  );
+
+  if (cityInput) {
+    return (
+      <div className="gotInput">
+        <div>{form}</div>
+        <div>{todayweather}</div>
       </div>
     );
   } else {
-    return form;
+    startAxiosCall("Cologne");
+    return (
+      <div className="onload">
+        <div>{form}</div>
+        <div>{todayweather}</div>
+      </div>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Searchform from "./Searchform";
-import Todayweather from "./Todayweather";
 import Forecast from "./Forecast";
 import Footer from "./Footer";
 
@@ -14,7 +13,6 @@ function App() {
         <div className="weather-app">
           <h1 className="d-none">React Weather App</h1>
           <Searchform />
-          <Todayweather />
           <hr />
           <Forecast />
         </div>
