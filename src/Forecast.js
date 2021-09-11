fa33bc4602f310c7773efa96dd2f0b0a546191d9
👩Ladybug🐞 <psyjulga@yahoo.de>
2021-09-11 19:46:49 +0200

    fix forecast bug with useeffect

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ForecastDay from "./ForecastDay";
 import "./Forecast.css";
 import axios from "axios";
@@ -6,6 +6,10 @@ import axios from "axios";
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecastData, setForecastData] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coords]);
 
   function handleResponse(response) {
     setForecastData(response.data.daily);
